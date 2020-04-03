c9a4755d78bd21970e1e28bb1d4ae4c000e6adcf
Aleksandar Stojkovski <aleksandar@stojkovski.ch>
2020-04-03 23:59:09 +0200

    various

--- Casetta/main.c
+++ Casetta/main.c
@@ -46,16 +46,19 @@ int DOOR_MOVING=0;
 int WIND_ON=0;
 int WIND_ANGLE=0;
 int WIND_TARGET_RANDOM_ANGLE=0;
+int TIME_PASSED=0;
+
+/* all the used and changable colors */
 Color BACKGROUND_COLOR={0.7f, 0.7f, 0.7f};
 Color ROOF_COLOR={1, 0, 0};
 Color DOOR_COLOR={0.5f, 0.25f, 0.25f};
 Color WALL_COLOR_INTERIOR={0.5f, 0.5f, 0.5f};
 Color WALL_COLOR_EXTERIOR={1, 1, 1};
 Color FLAG_COLOR={0, 0, 1};
-Color COMIGNOLO_UPPER_COLOR={0.5, 0.25, 0.25};
-Color COMIGNOLO_LOWER_COLOR={0.25, 0.25, 0.20};
-int TIME_PASSED=0;
+Color COMIGNOLO_UPPER_COLOR={0.5f, 0.25f, 0.25f};
+Color COMIGNOLO_LOWER_COLOR={0.25f, 0.25f, 0.20f};
 
+/* prints the time */
 void printTime(){
     char buff[100];
     time_t now = time(0);
@@ -63,10 +66,12 @@ void printTime(){
     printf("%s\n", buff);
 }
 
+/* returns random number in the specified range */
 int getRandoms(int lower, int upper){
         return (rand() %(upper - lower + 1)) + lower;
 }
 
+/* calculates the normal vector of the surface given three points */
 void normal_calculator(Point a, Point b, Point c, Point * destination_normal){
 
     Point ab = {b.x - a.x, b.y - a.y, b.z - a.z};
@@ -89,6 +94,7 @@ void normal_calculator(Point a, Point b, Point c, Point * destination_normal){
 
 
 void transMenuCB(int value){
+    // translation menu callback
     switch (value) {
         case 1:
             if (fabsf(X_POS) >= MAX_X_POS) return;
@@ -121,6 +127,7 @@ void transMenuCB(int value){
 }
 
 void colorMenuCB(int value) {
+    // color menu callback
     switch (value) {
         case 1:
             ROOF_COLOR.r=1;
@@ -159,6 +166,7 @@ void colorMenuCB(int value) {
 }
 
 void mainMenuCB(int value) {
+    // main menu callback
     switch (value) {
         case 1:
             SHOW_AXIS = !SHOW_AXIS;
@@ -170,15 +178,18 @@ void mainMenuCB(int value) {
         case 3:
             TODO:
             WIND_TARGET_RANDOM_ANGLE=getRandoms(0,360);
-            WIND_ON=1;
+            WIND_ON=!WIND_ON;
             break;
         default:
             break;
     }
+
     glutPostRedisplay();
 }
 
 void createMenu() {
+
+    // translation menu
     int translationMenu = glutCreateMenu(transMenuCB);
     glutAddMenuEntry("Translate +X",1);
     glutAddMenuEntry("Translate -X",2);
@@ -187,6 +198,7 @@ void createMenu() {
     glutAddMenuEntry("Translate +Z",5);
     glutAddMenuEntry("Translate -Z",6);
 
+    // color menu
     int colorMenu = glutCreateMenu(colorMenuCB);
     glutAddMenuEntry("Roof Red",1);
     glutAddMenuEntry("Roof Blue",2);
@@ -195,6 +207,7 @@ void createMenu() {
     glutAddMenuEntry("Wall Blue",5);
     glutAddMenuEntry("Wall White",6);
 
+    // main menu
     int mainMenu = glutCreateMenu(mainMenuCB);
     glutAddMenuEntry("Show/Hide Axis", 1);
     glutAddMenuEntry("Open/Close Door", 2);
@@ -204,27 +217,27 @@ void createMenu() {
 
     // attach the menu to the right button
     glutAttachMenu(GLUT_RIGHT_BUTTON);
+
 }
 
 
 void draw_triangle(Point points[3], Color color){
     glPolygonMode(GL_FRONT ,GL_FILL);
     glBegin(GL_TRIANGLES);
-    glColor3f(color.r,color.g,color.b);
-    glVertex3f(points[0].x,points[0].y,points[0].z);
-    glVertex3f(points[1].x,points[1].y,points[1].z);
-    glVertex3f(points[2].x,points[2].y,points[2].z);
+        glColor3f(color.r,color.g,color.b);
+        glVertex3f(points[0].x,points[0].y,points[0].z);
+        glVertex3f(points[1].x,points[1].y,points[1].z);
+        glVertex3f(points[2].x,points[2].y,points[2].z);
     glEnd();
 }
 
 void draw_rectangle(Point points[4], Color color){
 
-    // triangoli che creano la piramide
-
-    // GL_FRONT perche' vediamo solo il "front" delle facciate
-    // GL_FILL per fare in modo che il triangolo sia riempito di colore
+    // GL_FRONT because we only see the front of the triangle
+    // GL_FILL to fill the color of the rectangle
     glPolygonMode(GL_FRONT ,GL_FILL);
 
+    // draw triangles
     glBegin(GL_TRIANGLES);
     glColor3f(color.r,color.g,color.b);
     for (i=0;i<3;i++){
@@ -235,6 +248,7 @@ void draw_rectangle(Point points[4], Color color){
     glVertex3f(points[3].x,points[3].y,points[3].z);
     glEnd();
 
+    // draw triangles border
     if (SHOW_TRIANGLES){
         glPolygonMode(GL_FRONT ,GL_LINE);
         glBegin(GL_TRIANGLES);
@@ -252,83 +266,91 @@ void draw_rectangle(Point points[4], Color color){
 
 void draw_rectangle_back(Point points[4], Color color){
 
-    // triangoli che creano la piramide
-
-    // GL_FRONT perche' vediamo solo il "front" delle facciate
-    // GL_FILL per fare in modo che il triangolo sia riempito di colore
+    // GL_FRONT because we only see the front of the triangle
+    // GL_FILL to fill the color of the rectangle
     glPolygonMode(GL_FRONT ,GL_FILL);
 
+    // draw triangles
     glBegin(GL_TRIANGLES);
-    glColor3f(color.r,color.g,color.b);
-    for (i=2;i>=0;i--){
-        glVertex3f(points[i].x,points[i].y,points[i].z);
-    }
-    glVertex3f(points[0].x,points[0].y,points[0].z);
-    glVertex3f(points[3].x,points[3].y,points[3].z);
-    glVertex3f(points[2].x,points[2].y,points[2].z);
+        glColor3f(color.r,color.g,color.b);
+        for (i=2;i>=0;i--){
+            glVertex3f(points[i].x,points[i].y,points[i].z);
+        }
+        glVertex3f(points[0].x,points[0].y,points[0].z);
+        glVertex3f(points[3].x,points[3].y,points[3].z);
+        glVertex3f(points[2].x,points[2].y,points[2].z);
     glEnd();
 
+    // draw triangles border
     if (SHOW_TRIANGLES) {
         glPolygonMode(GL_FRONT, GL_LINE);
         glBegin(GL_TRIANGLES);
-        glColor3f(0, 0, 0);
-        for (i = 2; i >= 0; i--) {
-            glVertex3f(points[i].x, points[i].y, points[i].z);
-        }
-        glVertex3f(points[0].x, points[0].y, points[0].z);
-        glVertex3f(points[3].x, points[3].y, points[3].z);
-        glVertex3f(points[2].x, points[2].y, points[2].z);
+            glColor3f(0, 0, 0);
+            for (i = 2; i >= 0; i--) {
+                glVertex3f(points[i].x, points[i].y, points[i].z);
+            }
+            glVertex3f(points[0].x, points[0].y, points[0].z);
+            glVertex3f(points[3].x, points[3].y, points[3].z);
+            glVertex3f(points[2].x, points[2].y, points[2].z);
         glEnd();
     }
 
 }
 
-void draw_pol(Point points[4], float depth, Color external_color, Color internal_color){
+void draw_parallelepiped(Point *points, float depth, Color external_color, Color internal_color){
 
-    Point translated_in_z[4];
+    // points translated trough normal
+    Point translated_trough_normal[4];
+    // faces of the
     Point right_face[4];
     Point left_face[4];
-    Point top_face[4];
-    Point bottom_face[4];
+    Point front_face[4];
+    Point back_face[4];
 
+    // calculate normal vector
     Point normal = {0, 0, 0};
-
     normal_calculator(points[0], points[1], points[2], &normal);
 
-    // calcolo rettangolo spostano nell'asse Z
+    // calculate points translated trough normal
     for (i=0;i<4;i++){
-        translated_in_z[i]=points[i];
-        translated_in_z[i].x=translated_in_z[i].x + depth*normal.x;
-        translated_in_z[i].y=translated_in_z[i].y + depth*normal.y;
-        translated_in_z[i].z=translated_in_z[i].z + depth*normal.z;
+        translated_trough_normal[i]=points[i];
+        translated_trough_normal[i].x= translated_trough_normal[i].x + depth * normal.x;
+        translated_trough_normal[i].y= translated_trough_normal[i].y + depth * normal.y;
+        translated_trough_normal[i].z= translated_trough_normal[i].z + depth * normal.z;
     }
 
+    // right face
     right_face[0]=points[1];
-    right_face[1]=translated_in_z[1];
-    right_face[2]=translated_in_z[2];
+    right_face[1]=translated_trough_normal[1];
+    right_face[2]=translated_trough_normal[2];
     right_face[3]=points[2];
 
+    // left face
     left_face[0]=points[0];
     left_face[1]=points[3];
-    left_face[2]=translated_in_z[3];
-    left_face[3]=translated_in_z[0];
+    left_face[2]=translated_trough_normal[3];
+    left_face[3]=translated_trough_normal[0];
 
-    top_face[0]=points[3];
-    top_face[1]=points[2];
-    top_face[2]=translated_in_z[2];
-    top_face[3]=translated_in_z[3];
+    // front face
+    front_face[0]=points[3];
+    front_face[1]=points[2];
+    front_face[2]=translated_trough_normal[2];
+    front_face[3]=translated_trough_normal[3];
 
-    bottom_face[0]=points[0];
-    bottom_face[1]=translated_in_z[0];
-    bottom_face[2]=translated_in_z[1];
-    bottom_face[3]=points[1];
+    // back face
+    back_face[0]=points[0];
+    back_face[1]=translated_trough_normal[0];
+    back_face[2]=translated_trough_normal[1];
+    back_face[3]=points[1];
 
+    // draw all the pieces of the parallelepiped
     draw_rectangle(points, external_color);
-    draw_rectangle_back(translated_in_z, internal_color);
+    draw_rectangle_back(translated_trough_normal, internal_color);
     draw_rectangle(right_face, external_color);
     draw_rectangle(left_face, external_color);
-    draw_rectangle(top_face, external_color);
-    draw_rectangle(bottom_face, external_color);
+    draw_rectangle(front_face, external_color);
+    draw_rectangle(back_face, external_color);
+
 }
 
 void draw_axis() {
@@ -341,15 +363,15 @@ void draw_axis() {
     glLineStipple(1, 0xf0f0);
     // axes are ten units long.
     glBegin(GL_LINES);
-    // Draw a red x-axis
+    // sraw a red x-axis
     glColor3ub(255, 0, 0);
     glVertex3f(-10, 0, 0);
     glVertex3f(10, 0, 0);
-    // Draw a green y-axis
+    // sraw a green y-axis
     glColor3ub(0, 255, 0);
     glVertex3f(0, -10, 0);
     glVertex3f(0, 10, 0);
-    // Draw a blue z-axis
+    // sraw a blue z-axis
     glColor3ub(0, 0, 255);
     glVertex3f(0, 0, -10);
     glVertex3f(0, 0, 10);
@@ -360,11 +382,13 @@ void draw_axis() {
 
 void draw() {
 
-    // Background Color
+    // background Color
     glClearColor(BACKGROUND_COLOR.r, BACKGROUND_COLOR.g, BACKGROUND_COLOR.b, 0.0);
-    // Delete scene and apply defined Color
+
+    // delete scene and apply defined Color
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // NOLINT(hicpp-signed-bitwise)
 
+    // 3d vertexes of all the pieces of the house
     Point rect_front_right[4]={{0.5f, 0, 0}, {4, 0, 0}, {4, 2, 0}, {0.5f, 2, 0}};
     Point rect_front_left[4]={{-4, 0, 0}, {-0.5f, 0, 0}, {-0.5f, 2, 0}, {-4, 2, 0}};
     Point rect_front_center[4]={{-4, 2, 0}, {4, 2, 0}, {4, 4, 0}, {-4, 4, 0}};
@@ -372,8 +396,8 @@ void draw() {
     Point rect_right[4]={{4, 0, 0}, {4, 0, -8}, {4, 4, -8}, {4, 4, 0}};
     Point rect_left[4]={{-4, 4, 0}, {-4, 4, -8}, {-4, 0, -8}, {-4, 0, 0}};
     Point rect_bottom[4]={{-4, -0.1f, 0}, {-4, -0.1f, -8}, {4, -0.1f, -8}, {4, -0.1f, 0}};
-    Point front_triangle[3]={{-4, 4, 0}, {4, 4, 0}, {0, 6.4f, 0}};
-    Point back_triangle[3]={{-4, 4, -8}, {0, 6.4f, -8}, {4, 4, -8}};
+    Point front_triangle[3]={{-4, 4, 0}, {4, 4, 0}, {0, 6.5f, 0}};
+    Point back_triangle[3]={{-4, 4, -8}, {0, 6.5f, -8}, {4, 4, -8}};
     Point roof_left[4]={{-4.8f, 3.8f, 0.5f}, {0, 6.8f, 0.5f}, {0, 6.8f, -8.5f}, {-4.8f, 3.8f, -8.5f}};
     Point roof_right[4]={{4.8f, 3.8f, 0.5f}, {4.8f, 3.8f, -8.5f}, {0, 6.8f, -8.5f}, {0, 6.8f, 0.5f}};
     Point comignolo_pz1[4]={{-3, 4, -4}, {-2, 4, -4}, {-2, 7, -4}, {-3, 7, -4}};
@@ -382,57 +406,61 @@ void draw() {
     Point flag_triangle_front[3]={{0, -1, 0}, {1.5f, -0.5f, 0}, {0, 0, 0}};
     Point flag_triangle_back[3]={flag_triangle_front[0],flag_triangle_front[2],flag_triangle_front[1]};
 
-    // DRAW
+    // START DRAWING
 
     draw_axis();
 
     // Trasla la casa
     glPushMatrix();
 
+        // translate to X_POS Y_POS Z_POS
         glTranslatef(X_POS,Y_POS,Z_POS);
 
-        draw_pol(rect_front_right,-0.3f,WALL_COLOR_EXTERIOR,WALL_COLOR_INTERIOR);
-        draw_pol(rect_front_left,-0.3f,WALL_COLOR_EXTERIOR,WALL_COLOR_INTERIOR);
-        draw_pol(rect_front_center,-0.3f,WALL_COLOR_EXTERIOR,WALL_COLOR_INTERIOR);
-        draw_pol(rect_back,-0.3f,WALL_COLOR_EXTERIOR,WALL_COLOR_INTERIOR);
-        draw_pol(rect_right,-0.3f,WALL_COLOR_EXTERIOR,WALL_COLOR_INTERIOR);
-        draw_pol(rect_left,-0.3f,WALL_COLOR_EXTERIOR,WALL_COLOR_INTERIOR);
-        draw_pol(rect_bottom,-0.1f,WALL_COLOR_EXTERIOR,WALL_COLOR_INTERIOR);
+        // draw house
+        draw_parallelepiped(rect_front_right, -0.3f, WALL_COLOR_EXTERIOR, WALL_COLOR_INTERIOR);
+        draw_parallelepiped(rect_front_left, -0.3f, WALL_COLOR_EXTERIOR, WALL_COLOR_INTERIOR);
+        draw_parallelepiped(rect_front_center, -0.3f, WALL_COLOR_EXTERIOR, WALL_COLOR_INTERIOR);
+        draw_parallelepiped(rect_back, -0.3f, WALL_COLOR_EXTERIOR, WALL_COLOR_INTERIOR);
+        draw_parallelepiped(rect_right, -0.3f, WALL_COLOR_EXTERIOR, WALL_COLOR_INTERIOR);
+        draw_parallelepiped(rect_left, -0.3f, WALL_COLOR_EXTERIOR, WALL_COLOR_INTERIOR);
+        draw_parallelepiped(rect_bottom, -0.1f, WALL_COLOR_EXTERIOR, WALL_COLOR_INTERIOR);
         draw_triangle(front_triangle, WALL_COLOR_EXTERIOR);
         draw_triangle(back_triangle, WALL_COLOR_EXTERIOR);
-        draw_pol(roof_left,-0.3f,ROOF_COLOR,ROOF_COLOR);
-        draw_pol(roof_right,-0.3f,ROOF_COLOR,ROOF_COLOR);
-        draw_pol(comignolo_pz1, -1, COMIGNOLO_LOWER_COLOR, COMIGNOLO_LOWER_COLOR);
-        draw_pol(comignolo_pz2, -1.4f, COMIGNOLO_UPPER_COLOR, COMIGNOLO_UPPER_COLOR);
+        draw_parallelepiped(roof_left, -0.3f, ROOF_COLOR, ROOF_COLOR);
+        draw_parallelepiped(roof_right, -0.3f, ROOF_COLOR, ROOF_COLOR);
+        draw_parallelepiped(comignolo_pz1, -1, COMIGNOLO_LOWER_COLOR, COMIGNOLO_LOWER_COLOR);
+        draw_parallelepiped(comignolo_pz2, -1.4f, COMIGNOLO_UPPER_COLOR, COMIGNOLO_UPPER_COLOR);
 
-
+        // open/close door based on DOOR_ANGLE
         glPushMatrix();
-
             glTranslatef(0.5,0,0);
             glRotatef(DOOR_ANGLE,0,1,0);
             glTranslatef(0.5,0,0);
-            draw_pol(porta,-0.1f,DOOR_COLOR,DOOR_COLOR);
-
+            draw_parallelepiped(porta, -0.1f, DOOR_COLOR, DOOR_COLOR);
         glPopMatrix();
 
     glPopMatrix();
 
+    // flag
     glPushMatrix();
-
-    glTranslatef(X_POS+3,Y_POS+7,Z_POS-4);
-    glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
-    GLUquadricObj * obj = gluNewQuadric();
-    glColor3f(0, 0, 0);
-    gluCylinder(obj, 0.08f, 0.08f, 3, 30, 30);
-    glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
-
-    glRotatef(WIND_ANGLE, 0.0f, 1.0f, 0.0f);
-    draw_triangle(flag_triangle_front,FLAG_COLOR);
-    draw_triangle(flag_triangle_back,FLAG_COLOR);
-
+        glTranslatef(X_POS+3,Y_POS+7,Z_POS-4);
+        // rotate cilynder
+        glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+        glColor3f(0, 0, 0);
+        // draw lower part of flag (cylinder)
+        gluCylinder(gluNewQuadric(), 0.08f, 0.08f, 3, 30, 30);
+        // rotate back in order to draw the triangle
+        glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
+        // apply wind toration
+        glRotatef(WIND_ANGLE, 0.0f, 1.0f, 0.0f);
+        // draw upper part of flag - front
+        draw_triangle(flag_triangle_front,FLAG_COLOR);
+        // draw upper part of flag - back
+        draw_triangle(flag_triangle_back,FLAG_COLOR);
     glPopMatrix();
-    glutSwapBuffers();
 
+    // display everything
+    glutSwapBuffers();
 }
 
 void init(){
@@ -447,10 +475,11 @@ void init(){
     // modalita' standard di trasformazione, non va piu cambiata
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    // cambia la prospettiva
-    // gluPerspective(100,1,1,10);
+    // change perspective
     gluLookAt(0, 0, 3, 0, 0, 0, 0, 1, 0);
+    // create menu (right click)
     createMenu();
+    // init random
     srand(time(0));
 }
 
@@ -478,7 +507,7 @@ void keyboardS(int key, int x, int y) {
 
 }
 
-void spinDisplay(int id){
+void timer_function(int id){
 
     if (TIME_PASSED <100)
         TIME_PASSED++;
@@ -522,25 +551,23 @@ void spinDisplay(int id){
     }
 
     glutPostRedisplay();
-    glutTimerFunc(SPIN_SPEED, spinDisplay, 1);
+    glutTimerFunc(SPIN_SPEED, timer_function, 1);
 }
 
-
-// Ruota con r e ferma con s
 void keyboard(unsigned char key, int x, int y) {
     switch (key) {
-        //r
         case 114:
+            // r key
             if (!IS_SPINNING) {
                 IS_SPINNING = 1;
             }
             break;
-            //s
         case 115:
+            // s key
             IS_SPINNING = 0;
             break;
-            // esc
         case 27:
+            // esc key
             glutDestroyWindow(MAIN_WINDOW);
             exit(0);
         default:
@@ -560,23 +587,27 @@ void reshapeCB(int w, int h) {
 }
 
 int main(int argc, char** argv) {
-    // Init glut library and window management
+    // init glut library and window management
     glutInit(&argc, argv);
+    // set double buffer
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); // NOLINT(hicpp-signed-bitwise)
-    // grandezza finestra
+    // window size
     glutInitWindowSize(1000,750);
-    // Title function
+    // set title of window
     MAIN_WINDOW = glutCreateWindow("House by Aleksandar Stojkovski");
-    // init
+    // init all the variables
     init();
-    // Reshape
+    // reshape for window resizing
     glutReshapeFunc(reshapeCB);
-    // Draw function
+    // draw function
     glutDisplayFunc(draw);
+    // catch keyboard arrows
     glutSpecialFunc(keyboardS);
+    // catch keyboard keys
     glutKeyboardFunc(keyboard);
-    // fa partire il timer
-    glutTimerFunc(SPIN_SPEED, spinDisplay, 1);
-    glutMainLoop();					// loop infinito
+    // start timer
+    glutTimerFunc(SPIN_SPEED, timer_function, 1);
+    // start main infinite loop
+    glutMainLoop();
 }
 
