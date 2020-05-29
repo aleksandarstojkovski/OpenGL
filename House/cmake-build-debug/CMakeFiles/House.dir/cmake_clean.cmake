5851fa4844722f4b97eda2cbc253499775227d68
Aleksandar Stojkovski <aleksandar@stojkovski.ch>
2020-05-29 15:07:52 +0200

    s12

---
+++ House/cmake-build-debug/CMakeFiles/House.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/House.dir/lib/RgbImage.cpp.obj"
+  "CMakeFiles/House.dir/main.cpp.obj"
+  "House.exe"
+  "House.exe.manifest"
+  "House.pdb"
+  "libHouse.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/House.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
